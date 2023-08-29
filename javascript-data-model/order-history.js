ac4cebc5c45ea86d26caa67dba06f23318a3d920
Omid Asadi <omiddzedd@gmail.com>
2023-08-29 19:05:07 +0000

    unfinished javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,72 @@
+const orderHistory = [
+  {
+    'Order #': '114-3941689-8772232',
+    items: [
+      {
+        name: 'Javascript for impatient programmers',
+        Price: '$31.55',
+        'Producer/Author': 'Dr. Axel Rauschmayer',
+      },
+    ],
+    'Total paid': '$34.00',
+    'Shipped to': 'JS Masher',
+    'Date of order': 'August 4, 2020',
+    'Date of delivery:': 'Aug 8, 2020',
+    'Last day to return': 'Sep 7, 2020',
+  },
+  {
+    'Order #': '113-9984268-1280257',
+    items: [
+      {
+        name: 'Javascript for impatient programmers',
+        Price: '$31.55',
+        'Producer/Author': 'Dr. Axel Rauschmayer',
+      },
+    ],
+    'Product(s)': 'The Timeless Way of Building',
+    'Producer/Author': 'Christopher Alexander',
+    Price: '$41.33',
+    'Total paid': '$44.53',
+    'Shipped to': 'JS Masher',
+    'Date of order': 'July 19, 2020',
+    'Date of delivery:': 'July 20, 2020',
+    'Last day to return': 'Aug 19, 2020',
+  },
+  {
+    'Order #': '114-2875557-9059409',
+    items: [
+      {
+        name: 'Javascript for impatient programmers',
+        Price: '$31.55',
+        'Producer/Author': 'Dr. Axel Rauschmayer',
+      },
+    ],
+    'Product(s)':
+      'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+    'Producer/Author': '',
+    Price: '$15.98',
+    'Total paid': '$17.22',
+    'Shipped to': 'JS Masher',
+    'Date of order': 'July 4, 2020',
+    'Date of delivery:': 'July 7, 2020',
+    'Last day to return': 'Aug 5, 2020',
+  },
+  {
+    'Order #': '113-2883177-2648248',
+    items: [
+      {
+        name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        Price: '$94.95',
+        'Producer/Author': null,
+      },
+      { name: 'The Art of Sql', Price: '$33.99' },
+    ],
+    'Producer/Author': 'Dr. Axel Rauschmayer',
+    Price: '$31.55',
+    'Total paid': '$34.00',
+    'Shipped to': 'JS Masher',
+    'Date of order': 'August 4, 2020',
+    'Date of delivery:': 'Aug 8, 2020',
+    'Last day to return': 'Sep 7, 2020',
+  },
+];
